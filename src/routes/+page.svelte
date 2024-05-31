210ba888172735afe2123632d53f924ee24477f1
Jose Von Chong <javch1995@gmail.com>
2024-05-31 16:02:08 -0500

    header ready

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-[32px_auto_32px] grid-rows-[32px_auto_auto_1fr_32px] custom-768:grid-cols-[32px_auto_auto_32px] lg:grid-cols-[32px_1fr_auto_1fr_32px] lg:grid-rows-[32px_1fr_auto_1fr_32px]  gap-y-8">
+<div class="grid grid-cols-[32px_auto_32px] grid-rows-[32px_auto_auto_1fr_32px] custom-768:grid-cols-[32px_auto_auto_32px] custom-768:grid-rows-[32px_auto_1fr_auto_32px] lg:grid-cols-[32px_auto_1fr_auto_32px] lg:grid-rows-[32px_auto_1fr_auto_32px]   gap-y-16">
     <!-- Main Image -->
     <picture class="col-full row-span-full">
         <source media="(min-width: 1024px)" srcset="https://www.copaair.com/promotions/destinations/img/ddm-large-DDM-itso-panama24.jpg" class="w-full h-auto" />
@@ -6,24 +6,42 @@
         <img alt="" src="https://www.copaair.com/promotions/destinations/img/ddm-small-DDM-itso-panama24.jpg" class="w-full" />
     </picture>
 
-    <picture class="row-start-2 row-end-2 col-start-2 col-end-2 w-full flex custom-768:row-start-2 custom-768:row-end-2 custom-768:self-start custom-768:justify-start lg:row-start-4 lg:row-end-4 lg:col-star-2 lg:col-end-2 lg:self-end ">
-        
-        <img alt="" src="https://www.copaair.com/promotions/destinations/img/ddm-itsopanama-blue-es.svg" class="lg:w-7/12" />
-    </picture>
-
+    <img alt="" src="https://www.copaair.com/promotions/destinations/img/ddm-itsopanama-blue-es.svg" class=" fadeinTitle row-start-2 row-end-2 col-start-2 col-end-2  custom-768:row-start-2 custom-768:row-end-2 place-self-center custom-768:place-self-start custom-768:self-start custom-768:justify-start lg:row-start-2 lg:row-end-2 lg:self-start  lg:col-star-2 lg:col-end-2 !w-3/6  lg:w-7/12" />
     
 
-    <div class="row-start-3 row-end-3 col-start-2 mx-auto custom-768:col-start-2 custom-768:row-start-3  custom-768:row-end-3 custom-768:mx-0 lg:col-start-4 lg:row-start-3 lg:row-end-3 lg:mx-auto">
-        <a href="#destinoDelMes">
-            <button class="button font-sans text-grey-50">Reserva ya</button>
+    <div class="fadeinButton row-start-3 row-end-3 col-start-2 col-end-2 mx-auto custom-768:row-start-3 custom-768:row-end-3 custom-768:!mx-0 lg:!col-start-4 lg:!col-end-4 lg:row-start-4 lg:row-end-4 lg:!mx-auto lg:order-2">
+        <a href="https://www.google.com">
+            <button class="fadeinbutton button font-sans text-grey-50 ">Reserva ya</button>
         </a>
     </div>
-
-    <div class="flex row-start-4 col-start-2 col-end-2 items-end custom-768:col-start-3 custom-768:col-end-3 custom-768:row-start-4 custom-768:row-end-4 custom-768:self-end lg:row-start-2 lg:row-end-2 lg:col-start-4 lg:col-end-4 lg:self-end lg:justify-end ">
-        <img alt="" src="https://www.copaair.com/promotions/destinations/img/itso-panama-main-logo-ES.svg" class=" w-full lg:w-10/12 " >
-        </div>
-
+    
+    <div class="flex  row-start-4 col-start-2 col-end-2 items-end custom-768:!col-start-3 custom-768:!col-end-3 custom-768:row-start-3 custom-768:row-end-3 custom-768:self-end  lg:row-start-3 lg:row-end-3 lg:!col-start-4 lg:!col-end-4 lg:self-end lg:justify-end lg:order-1">
+        <img alt="" src="https://www.copaair.com/promotions/destinations/img/itso-panama-main-logo-ES.svg" class="fadeinPanama w-4/5 mx-auto ">
+    </div>
+    
 </div>
 
 
-	
+
+<div class="grid grid-cols-[32px_auto_32px] grid-rows-[32px_auto_auto_1fr_32px] custom-768:grid-cols-[32px_auto_auto_32px] custom-768:grid-rows-[32px_auto_1fr_auto_32px] lg:grid-cols-[32px_auto_1fr_auto_32px] lg:grid-rows-[32px_auto_1fr_auto_32px]   gap-y-16">
+    <!-- Main Image -->
+    <picture class="col-full row-span-full">
+        <source media="(min-width: 1024px)" srcset="https://www.copaair.com/promotions/destinations/img/ddm-large-DDM-itso-panama24.jpg" class="w-full h-auto" />
+        <source media="(min-width: 768px)" srcset="https://www.copaair.com/promotions/destinations/img/ddm-medium-DDM-itso-panama24.jpg" class="w-full h-auto" />
+        <img alt="" src="https://www.copaair.com/promotions/destinations/img/ddm-small-DDM-itso-panama24.jpg" class="w-full" />
+    </picture>
+
+    <img alt="" src="https://www.copaair.com/promotions/destinations/img/ddm-itsopanama-blue-es.svg" class=" fadeinTitle row-start-2 row-end-2 col-start-2 col-end-2  custom-768:row-start-2 custom-768:row-end-2 place-self-center custom-768:place-self-start custom-768:self-start custom-768:justify-start lg:row-start-2 lg:row-end-2 lg:self-start  lg:col-star-2 lg:col-end-2 !w-3/6  lg:w-7/12" />
+    
+
+    <div class="fadeinButton row-start-3 row-end-3 col-start-2 col-end-2 mx-auto custom-768:row-start-3 custom-768:row-end-3 custom-768:!mx-0 lg:!col-start-4 lg:!col-end-4 lg:row-start-4 lg:row-end-4 lg:!mx-auto lg:order-2">
+        <a href="#destinationOfTheMonth">
+            <button class="fadeinbutton button font-sans text-grey-50 ">Reserva ya</button>
+        </a>
+    </div>
+    
+    <div class="flex  row-start-4 col-start-2 col-end-2 items-end custom-768:!col-start-3 custom-768:!col-end-3 custom-768:row-start-3 custom-768:row-end-3 custom-768:self-end  lg:row-start-3 lg:row-end-3 lg:!col-start-4 lg:!col-end-4 lg:self-end lg:justify-end lg:order-1">
+        <img alt="" src="https://www.copaair.com/promotions/destinations/img/itso-panama-main-logo-EN.svg" class="fadeinPanama w-4/5 mx-auto ">
+    </div>
+    
+</div>
